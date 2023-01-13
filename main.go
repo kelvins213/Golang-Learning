b755f60190e84257f1c14c2cf3e4b14ad2659204
kelvins213 <kelvinotilio@gmail.com>
2023-01-13 17:05:49 -0300

    cleaning validation of user's inputs

--- main.go
+++ main.go
@@ -38,6 +38,40 @@ var remainingTickets uint = 50; //variable typed like uint are not allowed to re
 var userName string;
 var userTickets uint;
 
+
+func showMenu(){
+	var wish int;
+
+	
+	//bookings := [49] string{};
+	//var bookings = [50]string{"Nana", "Nicole", "Peter"};
+
+
+	//in Go, we only have the for loop
+	for (wish != 4 && remainingTickets != 0) {
+		fmt.Println("================> MENU <================");
+		fmt.Printf("1 - Buy tickets \n2 - List Buyers \n3 - List Remaining Tickets \n4 - Exit System \n: ");
+		fmt.Scan(&wish)
+
+		switch wish {
+			case 1:
+				userInteration();
+			case 2:
+				listBuyers();
+			case 3:
+				listRemainingTickets();
+			default:
+				endSystem();
+		}
+	}
+}
+
+func userInteration(){
+	getUserName();
+	getUserTickets();
+	validateUserInputs();
+}
+
 func getUserName() {
 	fmt.Printf("Please, tell me your name: ");
 	fmt.Scanf("%v", &userName); 
@@ -48,26 +82,36 @@ func getUserTickets() {
 	fmt.Scanf("%d", &userTickets); //o & serve para o compilador também ler o endereço de memória da variável userTickets
 }
 
-func validateUserInput() int{
-	var userNameLenght = len(userName);
-	return userNameLenght;
+func validateUserInputs(){
+	var userNameIsValid = len(userName) <= 2;
+	var ticketsAreValide = userTickets > remainingTickets;
+
+	if (!userNameIsValid && !ticketsAreValide) {
+		buyTickets();
+	} else {
+		if (userNameIsValid && ticketsAreValide) {
+			fmt.Printf("\nSorry, your name has less than 2 letters \n");
+			fmt.Printf("\nSorry, we only have %d tickets left \n", remainingTickets);
+			fmt.Printf("Try again \n\n");
+			showMenu();
+		} else {
+			if (userNameIsValid) {
+				fmt.Printf("\nSorry, your name has less than 2 letters \n\n");
+				showMenu();
+			} else {
+				fmt.Printf("\nSorry, we only have %d tickets left \n", remainingTickets);
+				fmt.Printf("Please, try again \n\n");
+				showMenu();
+			}
+		}
+	}
 }
 
-func buyTickets(userNameLenght int){
-	if (userNameLenght > 2) {
-		if (userTickets > remainingTickets) {
-			fmt.Printf("\nSorry, we only have %d tickets left \n", remainingTickets);
-		} else {
-			fmt.Println("Thanks for helping us!");
-			remainingTickets = remainingTickets - userTickets;
-			bookings = append(bookings, userName);
-		}
-	} else {
-		fmt.Println("Your name is not valid! Please, try again");
-		getUserName();	
-		var lenght = validateUserInput();
-		buyTickets(lenght);
-	}
+
+func buyTickets(){
+	remainingTickets = remainingTickets - userTickets;
+	bookings = append(bookings, userName);
+	fmt.Println("Your tickets has just been granted");
 }
 
 func listBuyers(){
@@ -96,34 +140,8 @@ func endSystem(){
 
 func main() {	
 	var conferenceName = "Go Conference";
-	var wish int;
-
-	
-	//bookings := [49] string{};
-	//var bookings = [50]string{"Nana", "Nicole", "Peter"};
-
 	fmt.Printf("Welcome to %s booking application \n", conferenceName);
-
-	//in Go, we only have the for loop
-	for (wish != 4 && remainingTickets != 0) {
-		fmt.Println("================> MENU <================");
-		fmt.Printf("1 - Buy tickets \n2 - List Buyers \n3 - List Remaining Tickets \n4 - Exit System \n : ");
-		fmt.Scan(&wish)
-
-		switch wish {
-			case 1:
-				getUserName();
-				getUserTickets();
-				var length = validateUserInput();
-				buyTickets(length);
-			case 2:
-				listBuyers();
-			case 3:
-				listRemainingTickets();
-			default:
-				endSystem();
-		}
-	}
+	showMenu();
 	//iteration on a list
 	/*
 		for index, nameBooking := range bookings {
