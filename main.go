e9f10e124ceeea698d3920cad1e3f49d8678cb69
kelvins213 <kelvinotilio@gmail.com>
2023-01-13 22:00:10 -0300

    new file in package main

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ To do it, you can write "package <package-name>"
 
 package main
 /*
-You need say where the application begins to the compiler.
+You must say where the application begins to the compiler.
 This entry function is always the func main(){}
 For each file, you must have only one main() function.
 */
 
 /*
-When we download Go settings, it already gives to us some packages we can use,
+When we download Go settings, it already gives us some packages we can use,
 like fmt, but you may have to install another, depending on your application
 Each package has functions we can use. 
 It's like a container with lots of functionalities
@@ -61,9 +61,13 @@ func showMenu(){
 			case 3:
 				listRemainingTickets();
 			default:
-				endSystem();
+				endMenu();
 		}
 	}
+}
+
+func endMenu(){
+	fmt.Println("Thanks, goodbye!");
 }
 
 func userInteration(){
@@ -72,71 +76,6 @@ func userInteration(){
 	validateUserInputs();
 }
 
-func getUserName() {
-	fmt.Printf("Please, tell me your name: ");
-	fmt.Scanf("%v", &userName); 
-}
-
-func getUserTickets() {
-	fmt.Printf("\nHow many tickets do you wish to buy? Answer: ");
-	fmt.Scanf("%d", &userTickets); //o & serve para o compilador também ler o endereço de memória da variável userTickets
-}
-
-func validateUserInputs(){
-	var userNameIsValid = len(userName) <= 2;
-	var ticketsAreValide = userTickets > remainingTickets;
-
-	if (!userNameIsValid && !ticketsAreValide) {
-		buyTickets();
-	} else {
-		if (userNameIsValid && ticketsAreValide) {
-			fmt.Printf("\nSorry, your name has less than 2 letters \n");
-			fmt.Printf("\nSorry, we only have %d tickets left \n", remainingTickets);
-			fmt.Printf("Try again \n\n");
-			showMenu();
-		} else {
-			if (userNameIsValid) {
-				fmt.Printf("\nSorry, your name has less than 2 letters \n\n");
-				showMenu();
-			} else {
-				fmt.Printf("\nSorry, we only have %d tickets left \n", remainingTickets);
-				fmt.Printf("Please, try again \n\n");
-				showMenu();
-			}
-		}
-	}
-}
-
-
-func buyTickets(){
-	remainingTickets = remainingTickets - userTickets;
-	bookings = append(bookings, userName);
-	fmt.Println("Your tickets has just been granted");
-}
-
-func listBuyers(){
-	var arrayLength = len(bookings);
-	if (arrayLength > 0) {
-		for index, buyer := range bookings {
-			//index => the position where buyer is arrenged in the bookings array
-			//buyer => its the element
-			fmt.Printf("%dº buyer: [%s] \n", index + 1, buyer);
-		}
-		/*
-		for _, buyer := range bookings{} the underline works to ignore the use of a expected variable
-		*/
-	} else {
-		fmt.Println("We don't have buyers yet!");
-	}
-}
-
-func listRemainingTickets(){
-	fmt.Printf("\n We still have %d tickets avaliable of %d tickets \n", remainingTickets, conferenceTickets);
-}
-
-func endSystem(){
-	fmt.Println("Thanks, goodbye!");
-}
 
 func main() {	
 	var conferenceName = "Go Conference";
